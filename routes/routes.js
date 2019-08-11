10a4bd2d6e4589cada0ec0b0409896955113aa16
Kevin Muriuki <kevin@Yozios-MacBook-Air.local>
2019-08-11 16:44:55 -0700

    base

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var models = require('../models');
-var User = models.User;
 
 //////////////////////////////// PUBLIC ROUTES ////////////////////////////////
 // Users who are not logged in can see these routes
@@ -10,25 +8,9 @@ router.get('/', function(req, res, next) {
   res.render('home');
 });
 
-///////////////////////////// END OF PUBLIC ROUTES /////////////////////////////
-
-router.use(function(req, res, next){
-  if (!req.user) {
-    res.redirect('/login');
-  } else {
-    return next();
-  }
+router.get('/landing', function(req, res, next) {
+  res.render('index');
 });
 
-//////////////////////////////// PRIVATE ROUTES ////////////////////////////////
-// Only logged in users can see these routes
-
-router.get('/protected', function(req, res, next) {
-  res.render('protectedRoute', {
-    username: req.user.username,
-  });
-});
-
-///////////////////////////// END OF PRIVATE ROUTES /////////////////////////////
 
 module.exports = router;
